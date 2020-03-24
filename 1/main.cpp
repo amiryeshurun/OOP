ebed9d4bad0225c4d61e8aef1c820c50633b5a75
amiryeshurun <amiryeshooroon@gmail.com>
2020-03-24 18:04:44 +0200

    add the task

---
+++ 1/main.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Amir Yeshurun on 2020-03-24.
+//
+
+
+/*
+ * Create the following classes:
+ *
+ * - Party
+ * - Party Member
+ * - Knesset
+ *
+ * Party should have:
+ * - party name (:string)
+ * - party leader (:Party Member)
+ * - number of members (:int)
+ * - an array of party members
+ *
+ * Party member should have:
+ * - name (:string)
+ * - age (:int)
+ * - party (:Party)
+ * - party idx (:int)
+ *
+ * Knesset should have:
+ * - number (:int)
+ * - PM (:Party Member)
+ * - speaker of the knesset (:Party Member) - cannot also be the PM
+ * - an array of parties
+ *
+ * Supported functionality required:
+ * - add a party member to the knesset by party name
+ * - delete a party member to the knesset by party name
+ * - ensure that the PM is not the speaker of the knesset
+ */
+
