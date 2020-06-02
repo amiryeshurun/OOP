d8721c971dd99d74f25f7ad46996050ea2ba75e0
amiry <amiry@appcard.com>
2020-06-02 17:39:58 +0300

    assignment for 9th lesson

---
+++ 9/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+/*
+ * Create the following classes:
+ * Device: screen size,  device name, price, memory, brand
+ * Laptop: keyboard size
+ * TouchableDevice: minimumPressure
+ * TouchableLaptap: Laptop + TouchableDevice
+ */
+
+
+// Create a polomorphic print function
+// Create an array of devices (inside the main function) and allow the user to choose between:
+// 1: insert a laptop
+// 2: insert a touchable device
+// 3: insert a touchable laptop
+// 4: show the current amount of touchable devices (including touchable laptops)
+// 5: same for laptops
+
+// The last two should be implemented as global functions
+
+
