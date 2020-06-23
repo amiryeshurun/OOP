572069b177c389cb1427a16b9d4cfc802440cae4
amiry <amiry@appcard.com>
2020-06-23 16:47:52 +0300

    12th lesson

---
+++ 12/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+/*
+ * Create a class, called stream. The class should have the following attributes:
+ * A Constructor, accepting a linked list of an unknown type.
+ * A map function, accepting a mapper function from the type T to a new, unknown type, K. The return value should be a stream of the new type, after applying the mapper function on all of the stream's objects.
+ * A removeIf cuntion, accepting a precidate and removing all of the matching objects.
+ * forEach function, accepting a function and applying it to all of the objects in the stream.
+*/
